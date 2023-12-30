c369e695bba1189a0b15713d683a30e5e3731e1e
EncodeTalker2003 <2100011032@stu.pku.edu.cn>
2023-12-30 16:57:03 +0800

    task1 runner

--- task1_codes/run1.py
+++ task1_codes/run1.py
@@ -5,5 +5,5 @@ elif os.fork():
 	os.system('python3 task1.py --device cuda:0 --prompt 1 > ../output/task1/result_multi_points.txt')
 elif os.fork():
 	os.system('python3 task1.py --device cuda:1 --prompt 2 > ../output/task1/result_bbox.txt')
-else:
+elif os.fork():
 	os.system('python3 task1.py --device cuda:1 --prompt 3 > ../output/task1/result_bbox_margin.txt')
